9307552a81e7aa348927ae6afd7a984169200f85
baesaa0304 <baesaa0304@naver.com>
2025-01-27 17:52:37 +0900

    4. 단어 뒤집기

---
+++ JavaAlgorithms/src/string/algorithms04/Main.java
@@ -0,0 +1,26 @@
+package string.algorithms04;
+
+import java.io.*;
+
+public class Main {
+	String solution(String s) {
+		String answer = "";
+		StringBuilder sb = new StringBuilder(s).reverse();
+		answer = sb.toString();
+		//System.out.println(answer);
+		return answer;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Main obj = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int i =0; i < T; i++) {
+			String word = br.readLine();
+			System.out.println(obj.solution(word));
+		}
+		br.close();
+
+	}
+}
+ 
