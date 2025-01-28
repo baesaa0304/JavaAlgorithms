d4abaf2eae55e8f4ab7af4764ff9a564ff2213f5
baesaa0304 <baesaa0304@naver.com>
2025-01-28 13:42:24 +0900

    7.회문 문자열

---
+++ JavaAlgorithms/src/string/algorithms07/Main.java
@@ -0,0 +1,30 @@
+package string.algorithms07;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    String solution(String s) {
+    	String answer = "";
+    	s = s.toLowerCase();
+    	StringBuilder sb = new StringBuilder(s).reverse();
+    	if(s.equals(sb.toString())) {
+    		answer  = "YES";
+    	}  
+    	else {
+    		answer = "NO";
+    	}
+        return answer;
+    }
+
+	public static void main(String[] args) throws IOException {
+		Main obj = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		String word = br.readLine(); 
+		System.out.println(obj.solution(word)); 
+
+		br.close();
+	}
+}
+ 
