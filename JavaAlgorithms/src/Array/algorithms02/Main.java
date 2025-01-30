d0b86035c687b528bc69055c89d8ff505bb3ab40
baesaa0304 <baesaa0304@naver.com>
2025-01-30 14:35:41 +0900

    2. 보이는 학생

---
+++ JavaAlgorithms/src/Array/algorithms02/Main.java
@@ -0,0 +1,35 @@
+package Array.algorithms02;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	int solution(int[] number , int n) {
+		int answer = 1;
+		int tall = number[0];
+		for(int i = 1; i < n; i++) {
+			if(tall < number[i]) {
+				answer++;
+				tall = number[i];
+			}
+		}
+		return answer;
+	}
+	public static void main(String[] args) throws IOException {
+		Main obj = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int number[] = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			number[i] = n;
+		}
+		
+		System.out.println(obj.solution(number, N));
+	}
+}
+ 
