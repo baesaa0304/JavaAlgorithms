b41e095edac1ee37827a1da943a969ae1e1bb83b
baesaa0304 <baesaa0304@naver.com>
2025-02-08 16:58:15 +0900

    4. 연속 부분수열

--- JavaAlgorithms/src/TwoPointers_SlidingWindow/algorithmss04/Main2.java
+++ JavaAlgorithms/src/TwoPointers_SlidingWindow/algorithmss04/Main2.java
@@ -3,27 +3,35 @@ package TwoPointers_SlidingWindow.algorithmss04;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main2 {
 	
 	int solution(int n , int m , int arr[]) {
-		int answer = 0  , idx = 0 , sum = 0;
+		int answer = 0;
 		for(int i = 0; i < n; i++) {
-			sum += arr[i];
-			if(sum == m) {
-				answer++;
-			}
-			while(m < sum) {
-				sum -= arr[idx++];
+			int sum = arr[i];
+			System.out.println("---------------------");
+			System.out.println("sum = " + sum);
+			for(int j = i + 1; j < n; j++) {
+				
+				sum+= arr[j];
+				System.out.println("j = " + arr[j]);
+				System.out.println("sum = " + sum);
 				if(sum == m) {
 					answer++;
+					System.out.println("answer = "  + answer);
+					break;
+				}
+				else if(sum > m) {
+					break;
 				}
 			}
+			
 		}
 		return answer;
 	}
 	
 	public static void main(String[] args) throws IOException {
-		Main obj = new Main();
+		Main2 obj = new Main2();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
